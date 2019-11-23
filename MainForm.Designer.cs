05995983a4b07717f981d603e1588fbe85d0818c
Celal Ergun <celalergun@gmail.com>
2019-11-23 15:21:45 +0300

    First commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace XmlDBTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCategory = new System.Windows.Forms.DataGridView();
+            this.CategoryIdColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniVeriTabanıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.veriTabanıAçToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.veriTabanınıKaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvJob = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DescriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.kategoriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniKategoriEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newJobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJob)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCategory
+            // 
+            this.dgvCategory.AllowUserToAddRows = false;
+            this.dgvCategory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dgvCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategory.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CategoryIdColumn,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvCategory.Location = new System.Drawing.Point(12, 27);
+            this.dgvCategory.Name = "dgvCategory";
+            this.dgvCategory.Size = new System.Drawing.Size(529, 410);
+            this.dgvCategory.TabIndex = 0;
+            // 
+            // CategoryIdColumn
+            // 
+            this.CategoryIdColumn.DataPropertyName = "CategoryId";
+            this.CategoryIdColumn.HeaderText = "CategoryId";
+            this.CategoryIdColumn.Name = "CategoryIdColumn";
+            this.CategoryIdColumn.Width = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "CategoryName";
+            this.Column1.HeaderText = "CategoryName";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 250;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "StartDate";
+            this.Column2.HeaderText = "StartDate";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "IsFinished";
+            this.Column3.HeaderText = "IsFinished";
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.kategoriToolStripMenuItem,
+            this.jobToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1158, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniVeriTabanıToolStripMenuItem,
+            this.veriTabanıAçToolStripMenuItem,
+            this.veriTabanınıKaydetToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.çıkışToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.dosyaToolStripMenuItem.Text = "File";
+            // 
+            // yeniVeriTabanıToolStripMenuItem
+            // 
+            this.yeniVeriTabanıToolStripMenuItem.Name = "yeniVeriTabanıToolStripMenuItem";
+            this.yeniVeriTabanıToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.yeniVeriTabanıToolStripMenuItem.Text = "New database...";
+            // 
+            // veriTabanıAçToolStripMenuItem
+            // 
+            this.veriTabanıAçToolStripMenuItem.Name = "veriTabanıAçToolStripMenuItem";
+            this.veriTabanıAçToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.veriTabanıAçToolStripMenuItem.Text = "Open database...";
+            this.veriTabanıAçToolStripMenuItem.Click += new System.EventHandler(this.openDatabase_Click);
+            // 
+            // veriTabanınıKaydetToolStripMenuItem
+            // 
+            this.veriTabanınıKaydetToolStripMenuItem.Name = "veriTabanınıKaydetToolStripMenuItem";
+            this.veriTabanınıKaydetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.veriTabanınıKaydetToolStripMenuItem.Text = "Save database as ...";
+            this.veriTabanınıKaydetToolStripMenuItem.Click += new System.EventHandler(this.SaveDatabase_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.çıkışToolStripMenuItem.Text = "Exit";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // dgvJob
+            // 
+            this.dgvJob.AllowUserToAddRows = false;
+            this.dgvJob.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvJob.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJob.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.Column5,
+            this.DescriptionColumn,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.dgvJob.Location = new System.Drawing.Point(547, 27);
+            this.dgvJob.Name = "dgvJob";
+            this.dgvJob.Size = new System.Drawing.Size(599, 292);
+            this.dgvJob.TabIndex = 2;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "JobId";
+            this.Column4.HeaderText = "JobId";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 5;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "CategoryId";
+            this.Column5.HeaderText = "CategoryId";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 5;
+            // 
+            // DescriptionColumn
+            // 
+            this.DescriptionColumn.DataPropertyName = "Description";
+            this.DescriptionColumn.HeaderText = "Description";
+            this.DescriptionColumn.Name = "DescriptionColumn";
+            this.DescriptionColumn.Width = 200;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "StartDate";
+            this.Column6.HeaderText = "StartDate";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "EndDate";
+            this.Column7.HeaderText = "EndDate";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.DataPropertyName = "IsFinished";
+            this.Column8.HeaderText = "IsFinished";
+            this.Column8.Name = "Column8";
+            this.Column8.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column8.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(547, 325);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(599, 112);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // kategoriToolStripMenuItem
+            // 
+            this.kategoriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniKategoriEkleToolStripMenuItem});
+            this.kategoriToolStripMenuItem.Name = "kategoriToolStripMenuItem";
+            this.kategoriToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.kategoriToolStripMenuItem.Text = "Category";
+            // 
+            // yeniKategoriEkleToolStripMenuItem
+            // 
+            this.yeniKategoriEkleToolStripMenuItem.Name = "yeniKategoriEkleToolStripMenuItem";
+            this.yeniKategoriEkleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.yeniKategoriEkleToolStripMenuItem.Text = "New category";
+            this.yeniKategoriEkleToolStripMenuItem.Click += new System.EventHandler(this.addNewCategory_Click);
+            // 
+            // jobToolStripMenuItem
+            // 
+            this.jobToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newJobToolStripMenuItem});
+            this.jobToolStripMenuItem.Name = "jobToolStripMenuItem";
+            this.jobToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.jobToolStripMenuItem.Text = "Job";
+            // 
+            // newJobToolStripMenuItem
+            // 
+            this.newJobToolStripMenuItem.Name = "newJobToolStripMenuItem";
+            this.newJobToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newJobToolStripMenuItem.Text = "New job";
+            this.newJobToolStripMenuItem.Click += new System.EventHandler(this.newJobToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 449);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dgvJob);
+            this.Controls.Add(this.dgvCategory);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Xml based database demo";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJob)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCategory;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniVeriTabanıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem veriTabanıAçToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem veriTabanınıKaydetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgvJob;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CategoryIdColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DescriptionColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column8;
+        private System.Windows.Forms.ToolStripMenuItem kategoriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniKategoriEkleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jobToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newJobToolStripMenuItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XmlDBTest
+{
+    public partial class MainForm : Form
+    {
+        // we are using the dataset as our database
+        DataSet _dataBase = null;
+        
+        // category table and the binding source
+        DataTable _categoryTable = null;
+        BindingSource _categoryBS = null;
+
+        // jobs table and the binding source
+        DataTable _jobTable = null;
+        BindingSource _jobBS = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            // Create a dataset to hold our database in memory
+            _dataBase = new DataSet("JobsDatabase");
+
+            // Create a category table
+            _categoryTable = new DataTable("Categories");
+            _categoryTable.Columns.Add(new DataColumn("CategoryId", typeof(Int32)));
+            _categoryTable.Columns.Add(new DataColumn("CategoryName", typeof(string)));
+            _categoryTable.Columns.Add(new DataColumn("StartDate", typeof(DateTime)));
+            _categoryTable.Columns.Add(new DataColumn("IsFinished", typeof(bool)));
+            // Set the primary key
+            _categoryTable.PrimaryKey = new DataColumn[] { _categoryTable.Columns["CategoryId"] };
+            // Make it autoincrement, 1 by 1
+            _categoryTable.Columns["CategoryId"].AutoIncrement = true;
+            _categoryTable.Columns["CategoryId"].AutoIncrementSeed = 1;
+            // Add the table to the database
+            _dataBase.Tables.Add(_categoryTable);
+
+            _jobTable = new DataTable("Jobs");
+            _jobTable.Columns.Add(new DataColumn("JobId", typeof(Int32)));
+            _jobTable.Columns.Add(new DataColumn("CategoryId", typeof(Int32)));
+            _jobTable.Columns.Add(new DataColumn("Description", typeof(string)));
+            _jobTable.Columns.Add(new DataColumn("StartDate", typeof(DateTime)));
+            _jobTable.Columns.Add(new DataColumn("EndDate", typeof(DateTime)));
+            _jobTable.Columns.Add(new DataColumn("IsFinished", typeof(bool)));
+            _jobTable.PrimaryKey = new DataColumn[] { _jobTable.Columns["JobId"] };
+
+            // Fantasize with auto increment seed, starts from 100, increases 3 by 3
+            _jobTable.Columns["JobId"].AutoIncrement = true;
+            _jobTable.Columns["JobId"].AutoIncrementSeed = 100;
+            _jobTable.Columns["JobId"].AutoIncrementStep = 3;
+            _dataBase.Tables.Add(_jobTable);
+
+            // Set table relations
+            _dataBase.Relations.Add("CategoryJob", _categoryTable.Columns["CategoryId"], _jobTable.Columns["CategoryId"]);
+            _dataBase.Relations["CategoryJob"].Nested = true;
+
+            // Create a binding source and set it as a datasource for our grid
+            _categoryBS = new BindingSource();
+            _categoryBS.DataSource = _categoryTable;
+            dgvCategory.DataSource = _categoryBS;
+
+            _jobBS = new BindingSource();
+            _jobBS.DataSource = _jobTable;
+            dgvJob.DataSource = _jobBS;
+
+            // set RTB data binding
+            richTextBox1.DataBindings.Add("Text", _jobBS, "Description");
+
+            // filter jobs according to the category
+            _categoryBS.CurrentChanged += _categoryBS_CurrentChanged;
+
+            // make grids faster
+            SetGridDoubleBufferingOn(dgvCategory);
+            SetGridDoubleBufferingOn(dgvJob);
+        }
+
+        private void _categoryBS_CurrentChanged(object sender, EventArgs e)
+        {
+            if (_categoryBS.Current == null)
+                _jobBS.Filter = "";
+            else
+                _jobBS.Filter = "CategoryId=" + (_categoryBS.Current as DataRowView).Row["CategoryId"];
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void SaveDatabase_Click(object sender, EventArgs e)
+        {
+            dgvCategory.EndEdit();
+            dgvJob.EndEdit();
+            using (SaveFileDialog sd = new SaveFileDialog())
+            {
+                sd.Filter = "XML files (*.xml)|*.xml";
+                var res = sd.ShowDialog();
+
+                if (res == DialogResult.OK)
+                {
+                    _dataBase.WriteXml(sd.FileName);
+                }
+            }
+        }
+
+        // get the current category Id for our new job
+        private void newJobToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_categoryBS.Current == null)
+                return;
+
+            var row = (DataRowView)_jobBS.AddNew();
+            row["CategoryID"] = (_categoryBS.Current as DataRowView).Row["CategoryID"];
+            row["StartDate"] = DateTime.Now;
+        }
+
+        private void addNewCategory_Click(object sender, EventArgs e)
+        {
+            var row = (DataRowView)_categoryBS.AddNew();
+            row["StartDate"] = DateTime.Now;
+        }
+
+        private void openDatabase_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog od = new OpenFileDialog())
+            {
+                od.Filter = "XML files (*.xml)|*.xml";
+                var res = od.ShowDialog();
+
+                if (res == DialogResult.OK)
+                {
+                    _dataBase.ReadXml(od.FileName);
+                }
+            }
+        }
+        public void SetGridDoubleBufferingOn(DataGridView dgv)
+        {
+            // Double buffering can make DGV slow in remote desktop
+            if (!SystemInformation.TerminalServerSession)
+            {
+                typeof(DataGridView).InvokeMember(
+                   "DoubleBuffered",
+                   BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetProperty,
+                   null,
+                   dgv,
+                   new object[] { true });
+            }
+        }
+    }
+}
