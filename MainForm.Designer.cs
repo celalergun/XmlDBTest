791dd51f2295cd2411fc13155012e4e5d4302f32
CelalErgun <celal.ergun@mehmetkocak.com>
2020-05-06 13:35:59 +0300

    GUI changes

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -40,6 +40,10 @@
             this.veriTabanınıKaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kategoriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniKategoriEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newJobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dgvJob = new System.Windows.Forms.DataGridView();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -48,10 +52,6 @@
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column8 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.kategoriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.yeniKategoriEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.jobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newJobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).BeginInit();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvJob)).BeginInit();
@@ -68,22 +68,26 @@
             this.Column1,
             this.Column2,
             this.Column3});
-            this.dgvCategory.Location = new System.Drawing.Point(12, 27);
+            this.dgvCategory.Location = new System.Drawing.Point(18, 42);
+            this.dgvCategory.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvCategory.Name = "dgvCategory";
-            this.dgvCategory.Size = new System.Drawing.Size(529, 410);
+            this.dgvCategory.RowHeadersWidth = 62;
+            this.dgvCategory.Size = new System.Drawing.Size(794, 631);
             this.dgvCategory.TabIndex = 0;
             // 
             // CategoryIdColumn
             // 
             this.CategoryIdColumn.DataPropertyName = "CategoryId";
             this.CategoryIdColumn.HeaderText = "CategoryId";
+            this.CategoryIdColumn.MinimumWidth = 8;
             this.CategoryIdColumn.Name = "CategoryIdColumn";
-            this.CategoryIdColumn.Width = 5;
+            this.CategoryIdColumn.Width = 8;
             // 
             // Column1
             // 
             this.Column1.DataPropertyName = "CategoryName";
             this.Column1.HeaderText = "CategoryName";
+            this.Column1.MinimumWidth = 8;
             this.Column1.Name = "Column1";
             this.Column1.Width = 250;
             // 
@@ -91,25 +95,31 @@
             // 
             this.Column2.DataPropertyName = "StartDate";
             this.Column2.HeaderText = "StartDate";
+            this.Column2.MinimumWidth = 8;
             this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
             // 
             // Column3
             // 
             this.Column3.DataPropertyName = "IsFinished";
             this.Column3.HeaderText = "IsFinished";
+            this.Column3.MinimumWidth = 8;
             this.Column3.Name = "Column3";
             this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Column3.Width = 150;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.dosyaToolStripMenuItem,
             this.kategoriToolStripMenuItem,
             this.jobToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1158, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1737, 35);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -122,41 +132,72 @@
             this.toolStripSeparator1,
             this.çıkışToolStripMenuItem});
             this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
-            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
             this.dosyaToolStripMenuItem.Text = "File";
             // 
             // yeniVeriTabanıToolStripMenuItem
             // 
             this.yeniVeriTabanıToolStripMenuItem.Name = "yeniVeriTabanıToolStripMenuItem";
-            this.yeniVeriTabanıToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.yeniVeriTabanıToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.yeniVeriTabanıToolStripMenuItem.Text = "New database...";
+            this.yeniVeriTabanıToolStripMenuItem.Click += new System.EventHandler(this.yeniVeriTabanıToolStripMenuItem_Click);
             // 
             // veriTabanıAçToolStripMenuItem
             // 
             this.veriTabanıAçToolStripMenuItem.Name = "veriTabanıAçToolStripMenuItem";
-            this.veriTabanıAçToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.veriTabanıAçToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.veriTabanıAçToolStripMenuItem.Text = "Open database...";
             this.veriTabanıAçToolStripMenuItem.Click += new System.EventHandler(this.openDatabase_Click);
             // 
             // veriTabanınıKaydetToolStripMenuItem
             // 
             this.veriTabanınıKaydetToolStripMenuItem.Name = "veriTabanınıKaydetToolStripMenuItem";
-            this.veriTabanınıKaydetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.veriTabanınıKaydetToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.veriTabanınıKaydetToolStripMenuItem.Text = "Save database as ...";
             this.veriTabanınıKaydetToolStripMenuItem.Click += new System.EventHandler(this.SaveDatabase_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(267, 6);
             // 
             // çıkışToolStripMenuItem
             // 
             this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
             this.çıkışToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.çıkışToolStripMenuItem.Text = "Exit";
             this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // kategoriToolStripMenuItem
+            // 
+            this.kategoriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniKategoriEkleToolStripMenuItem});
+            this.kategoriToolStripMenuItem.Name = "kategoriToolStripMenuItem";
+            this.kategoriToolStripMenuItem.Size = new System.Drawing.Size(100, 29);
+            this.kategoriToolStripMenuItem.Text = "Category";
+            // 
+            // yeniKategoriEkleToolStripMenuItem
+            // 
+            this.yeniKategoriEkleToolStripMenuItem.Name = "yeniKategoriEkleToolStripMenuItem";
+            this.yeniKategoriEkleToolStripMenuItem.Size = new System.Drawing.Size(223, 34);
+            this.yeniKategoriEkleToolStripMenuItem.Text = "New category";
+            this.yeniKategoriEkleToolStripMenuItem.Click += new System.EventHandler(this.addNewCategory_Click);
+            // 
+            // jobToolStripMenuItem
+            // 
+            this.jobToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newJobToolStripMenuItem});
+            this.jobToolStripMenuItem.Name = "jobToolStripMenuItem";
+            this.jobToolStripMenuItem.Size = new System.Drawing.Size(56, 29);
+            this.jobToolStripMenuItem.Text = "Job";
+            // 
+            // newJobToolStripMenuItem
+            // 
+            this.newJobToolStripMenuItem.Name = "newJobToolStripMenuItem";
+            this.newJobToolStripMenuItem.Size = new System.Drawing.Size(180, 34);
+            this.newJobToolStripMenuItem.Text = "New job";
+            this.newJobToolStripMenuItem.Click += new System.EventHandler(this.newJobToolStripMenuItem_Click);
             // 
             // dgvJob
             // 
@@ -171,29 +212,34 @@
             this.Column6,
             this.Column7,
             this.Column8});
-            this.dgvJob.Location = new System.Drawing.Point(547, 27);
+            this.dgvJob.Location = new System.Drawing.Point(820, 42);
+            this.dgvJob.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvJob.Name = "dgvJob";
-            this.dgvJob.Size = new System.Drawing.Size(599, 292);
+            this.dgvJob.RowHeadersWidth = 62;
+            this.dgvJob.Size = new System.Drawing.Size(898, 449);
             this.dgvJob.TabIndex = 2;
             // 
             // Column4
             // 
             this.Column4.DataPropertyName = "JobId";
             this.Column4.HeaderText = "JobId";
+            this.Column4.MinimumWidth = 8;
             this.Column4.Name = "Column4";
-            this.Column4.Width = 5;
+            this.Column4.Width = 8;
             // 
             // Column5
             // 
             this.Column5.DataPropertyName = "CategoryId";
             this.Column5.HeaderText = "CategoryId";
+            this.Column5.MinimumWidth = 8;
             this.Column5.Name = "Column5";
-            this.Column5.Width = 5;
+            this.Column5.Width = 8;
             // 
             // DescriptionColumn
             // 
             this.DescriptionColumn.DataPropertyName = "Description";
             this.DescriptionColumn.HeaderText = "Description";
+            this.DescriptionColumn.MinimumWidth = 8;
             this.DescriptionColumn.Name = "DescriptionColumn";
             this.DescriptionColumn.Width = 200;
             // 
@@ -201,21 +247,27 @@
             // 
             this.Column6.DataPropertyName = "StartDate";
             this.Column6.HeaderText = "StartDate";
+            this.Column6.MinimumWidth = 8;
             this.Column6.Name = "Column6";
+            this.Column6.Width = 150;
             // 
             // Column7
             // 
             this.Column7.DataPropertyName = "EndDate";
             this.Column7.HeaderText = "EndDate";
+            this.Column7.MinimumWidth = 8;
             this.Column7.Name = "Column7";
+            this.Column7.Width = 150;
             // 
             // Column8
             // 
             this.Column8.DataPropertyName = "IsFinished";
             this.Column8.HeaderText = "IsFinished";
+            this.Column8.MinimumWidth = 8;
             this.Column8.Name = "Column8";
             this.Column8.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.Column8.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Column8.Width = 150;
             // 
             // richTextBox1
             // 
@@ -223,55 +275,27 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.richTextBox1.Location = new System.Drawing.Point(547, 325);
+            this.richTextBox1.Location = new System.Drawing.Point(820, 500);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
-            this.richTextBox1.Size = new System.Drawing.Size(599, 112);
+            this.richTextBox1.Size = new System.Drawing.Size(896, 170);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             this.richTextBox1.WordWrap = false;
             // 
-            // kategoriToolStripMenuItem
+            // MainForm
             // 
-            this.kategoriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.yeniKategoriEkleToolStripMenuItem});
-            this.kategoriToolStripMenuItem.Name = "kategoriToolStripMenuItem";
-            this.kategoriToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
-            this.kategoriToolStripMenuItem.Text = "Category";
-            // 
-            // yeniKategoriEkleToolStripMenuItem
-            // 
-            this.yeniKategoriEkleToolStripMenuItem.Name = "yeniKategoriEkleToolStripMenuItem";
-            this.yeniKategoriEkleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.yeniKategoriEkleToolStripMenuItem.Text = "New category";
-            this.yeniKategoriEkleToolStripMenuItem.Click += new System.EventHandler(this.addNewCategory_Click);
-            // 
-            // jobToolStripMenuItem
-            // 
-            this.jobToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.newJobToolStripMenuItem});
-            this.jobToolStripMenuItem.Name = "jobToolStripMenuItem";
-            this.jobToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.jobToolStripMenuItem.Text = "Job";
-            // 
-            // newJobToolStripMenuItem
-            // 
-            this.newJobToolStripMenuItem.Name = "newJobToolStripMenuItem";
-            this.newJobToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.newJobToolStripMenuItem.Text = "New job";
-            this.newJobToolStripMenuItem.Click += new System.EventHandler(this.newJobToolStripMenuItem_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1158, 449);
+            this.ClientSize = new System.Drawing.Size(1737, 691);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.dgvJob);
             this.Controls.Add(this.dgvCategory);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
             this.Text = "Xml based database demo";
             ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).EndInit();
             this.menuStrip1.ResumeLayout(false);
